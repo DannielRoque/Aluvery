c6d5e5ea4742e75fad7c1b912c85f2c207dc2d5c
Daniel Roque <P0654608@portoseguro.com.br>
2025-04-26 01:35:38 -0300

    Criando o Fab utilizando o Scaffold do Material e iniciando a nova activity

--- app/src/main/java/com/example/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/ui/activities/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.aluvery.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.aluvery.model.Product
 import com.example.aluvery.sampleData.sampleCandies
 import com.example.aluvery.sampleData.sampleDrinks
@@ -17,21 +27,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            App(onFabClick = {
+                startActivity(Intent(this, ProductFormActivity::class.java))
+            })
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(onFabClick: () -> Unit = {}) {
     AluveryTheme {
         Surface {
-            HomeScreen(sectionalList)
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = onFabClick) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    HomeScreen(sectionalList)
+                }
+            }
         }
     }
 }
 
-val sectionalList : Map< String, List<Product>> = mapOf(
+val sectionalList: Map<String, List<Product>> = mapOf(
     "Promoções" to sampleCandies,
     "Salgados" to sampleDrinks,
     "Bebidas" to sampleCandies
